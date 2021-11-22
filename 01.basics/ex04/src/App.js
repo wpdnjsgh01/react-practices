cf6649acdff3661d78a0b008bc119fb2d39cf4a1
wpdnjsgh01 <wpdnjsgh02@naver.com>
2021-11-22 11:24:55 +0900

    20211122

---
+++ 01.basics/ex04/src/App.js
@@ -0,0 +1,6 @@
+import { myComponent } from './components/myComponent'
+const App = function() {
+    const app = document.createElement('h1');
+    app.textContent = 'Hello World';
+    return app;
+}
---
+++ 01.basics/webpack-practices/ex02/webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+
+    entry: path.resolve('src/index.js'),
+    output: {
+        path: path.resolve('public'),
+        filename: 'bundle.js'
+    }
+
+}
